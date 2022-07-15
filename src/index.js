06c59773e3ccc67982993d8b2dbeb29f75f251d9
pablo-lucas <pablo.lucv@gmail.com>
2022-07-14 20:31:12 -0500

    Git added children container

--- src/index.js
+++ src/index.js
@@ -3,29 +3,35 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import CommentDetail from "./CommentDetail";
 import ApprovalCard from "./ApprovalCard";
+import "./style/App.css";
 
 const App = () => {
   return (
     <div className="ui container comments">
-    <ApprovalCard/>
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        avatar={faker.image.avatar()}
-        content="Some content"
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 2:50PM"
-        avatar={faker.image.avatar()}
-        content="Some content"
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Today at 1:45PM"
-        avatar={faker.image.avatar()}
-        content="Some content"
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          avatar={faker.image.avatar()}
+          content="Some content"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:50PM"
+          avatar={faker.image.avatar()}
+          content="Some content"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Today at 1:45PM"
+          avatar={faker.image.avatar()}
+          content="Some content"
+        />
+      </ApprovalCard>
     </div>
   );
 };
